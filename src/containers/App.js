58b069527b7e79f755f4b1f8f29d18a2d8b765e3
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-19 20:01:19 +0100

    integrate face recognition api (wip)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
 
 import './App.css';
+
+// Add your API key here from https://www.clarifai.com/developer
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_API_KEY,
+});
 
 const particlesOptions = {
   particles: {
@@ -20,15 +27,44 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    };
+  }
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
