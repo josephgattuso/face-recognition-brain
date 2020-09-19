32a6fd9c0368e9256505d530d9ce420aaa406043
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-19 21:33:43 +0100

    add styles to face recognition feature

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img src={imageUrl} width='500px' height='auto' alt='' />
+        <img
+          alt=''
+          height='auto'
+          id='inputimage'
+          src={imageUrl}
+          width='500px'
+        />
+        <div
+          className='bounding-box'
+          style={{
+            bottom: box.bottomRow,
+            left: box.leftCol,
+            right: box.rightCol,
+            top: box.topRow,
+          }}
+        ></div>
       </div>
     </div>
   );
--- src/containers/App.js
+++ src/containers/App.js
@@ -32,8 +32,29 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
+      route: 'signin',
     };
   }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -41,16 +62,13 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
   };
 
   render() {
