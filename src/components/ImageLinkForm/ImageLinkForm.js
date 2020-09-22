2b56f6794ad58e99b5ed084c37848e0942f07031
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-22 10:35:25 +0100

    minor updates to the ui

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,16 +4,13 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className='f3'>
-        {
-          'This magic brain can detect human faces in pictures. Go on then, Git it a try.'
-        }
-      </p>
+      <p className='f3'>{'This web app can detect faces in pictures'}</p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input
             className='f4 pa2 w-70 center'
-            type='tex'
+            type='text'
+            placeholder='Enter an image URL link here...'
             onChange={onInputChange}
           />
           <button
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>{'Hi Demo User! Your current rank is:'}</div>
-      <div className='white f1'>{'#5'}</div>
+      <div className='white f3'>{`Hi ${name}, your current rank is:`}</div>
+      <div className='white f1 '>{`#${entries} entries!`}</div>
     </div>
   );
 };
