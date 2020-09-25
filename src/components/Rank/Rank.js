77fbbbd87b74c65bfe0f4e23b820deb8c79da3a7
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-25 17:29:26 +0100

    minor update to rank ui

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>{`Hi ${name}, your current rank is:`}</div>
-      <div className='white f1 '>{`#${entries} entries!`}</div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1 '>{`${entries}`}</div>
     </div>
   );
 };
